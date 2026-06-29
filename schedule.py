282f83cbf868bab166c5d5eed7012eb916442f13
Amogh <263152362+amoghcode@users.noreply.github.com>
2026-06-29 13:14:31 +0530

    Add add_task and add_event functions

--- schedule.py
+++ schedule.py
@@ -11,8 +11,22 @@ def save_db(filename, data):
 
 data1 = load_db("schedule.json")
 
+'''
 print(data1)
 data1["tasks"].append({"name": "maths assignment", "deadline": "10th June", "priority": "urgent"})
 save_db("schedule.json",data1)
+'''
 
+def add_task(filename, name, deadline, priority):
+    data = load_db(filename)
 
+    data["tasks"].append({"name":name,"deadline":deadline,"priority":priority})
+
+    save_db(filename,data)
+
+def add_event(filename,name, date, time, duration):
+    data = load_db(filename)
+
+    data["events"].append({"name":name,"date":date,"time":time,"duration":duration})
+
+    save_db(filename,data)
