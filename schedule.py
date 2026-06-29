1ce9ce00321c5711dad8066c523c06d5da2994d8
Amogh <263152362+amoghcode@users.noreply.github.com>
2026-06-29 13:31:06 +0530

    Add list_task and read_calendar functions

--- schedule.py
+++ schedule.py
@@ -9,14 +9,6 @@ def save_db(filename, data):
     with open(filename,"w") as file:
         json.dump(data, file)
 
-data1 = load_db("schedule.json")
-
-'''
-print(data1)
-data1["tasks"].append({"name": "maths assignment", "deadline": "10th June", "priority": "urgent"})
-save_db("schedule.json",data1)
-'''
-
 def add_task(filename, name, deadline, priority):
     data = load_db(filename)
 
@@ -30,3 +22,11 @@ def add_event(filename,name, date, time, duration):
     data["events"].append({"name":name,"date":date,"time":time,"duration":duration})
 
     save_db(filename,data)
+
+def read_calendar(filename):
+    data = load_db(filename)
+    return data["events"]
+
+def list_tasks(filename):
+    data = load_db(filename)
+    return data["tasks"]
