a50d6aecd332c70ba2398190a4d8d39cdecf705b
Amogh <263152362+amoghcode@users.noreply.github.com>
2026-07-03 22:12:21 +0530

    Add find_free_slots function

--- schedule.py
+++ schedule.py
@@ -1,4 +1,5 @@
 import json 
+from datetime import datetime, timedelta
 
 def load_db(filename):
     with open(filename) as file:
@@ -31,3 +32,28 @@ def list_tasks(filename):
     data = load_db(filename)
     return data["tasks"]
 
+def find_free_slots(filename, duration, day_start, day_end):
+    data = load_db(filename)
+    events = data["events"]
+    events = sorted(events, key=lambda event: datetime.strptime(event["time"], "%H:%M"))
+
+    day_start = datetime.strptime(day_start, "%H:%M")
+    day_end = datetime.strptime(day_end, "%H:%M")
+    duration = timedelta(hours=duration)
+
+    free_slots = []
+    current_time = day_start
+
+    for event in events:
+        event_start = datetime.strptime(event["time"], "%H:%M")
+        event_end = event_start + timedelta(hours=event["duration"])
+        
+        if event_start - current_time >= duration:
+            free_slots.append((current_time.strftime("%H:%M"),event_start.strftime("%H:%M")))
+
+        current_time = max(current_time, event_end)
+        
+    if day_end - current_time >= duration:
+        free_slots.append((current_time.strftime("%H:%M"), day_end.strftime("%H:%M")))
+
+    return free_slots
